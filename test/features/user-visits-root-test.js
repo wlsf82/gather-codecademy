4e8ee5b5c491c148e49369d9dcf486d07ec8d65f
Walmyr Filho <wlsf82@gmail.com>
2017-12-14 21:29:41 +0100

    create base project

---
+++ test/features/user-visits-root-test.js
@@ -0,0 +1,20 @@
+const {assert} = require('chai');
+
+describe('User visits root', () => {
+  describe('without existing items', () => {
+    it('starts blank', () => {
+      browser.url('/');
+      assert.equal(browser.getText('#items-container'), '');
+    });
+  });
+
+  describe('navigation', () => {
+    it('go to create page', () => {
+      browser.url('/');
+
+      browser.click('a[href="create.html"]');
+
+      assert.equal(browser.getText('h1'), 'Create');
+    });
+  });
+});
