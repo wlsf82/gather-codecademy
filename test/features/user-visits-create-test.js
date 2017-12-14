de2945951a13de4a299b1cf54da7a7b278005b7a
Walmyr Filho <wlsf82@gmail.com>
2017-12-14 22:15:07 +0100

    add test suite for create page

--- test/features/user-visits-create-test.js
+++ test/features/user-visits-create-test.js
@@ -1,5 +1,21 @@
-const {assert} = require('chai');
-const {buildItemObject} = require('../test-utils');
+const { assert } = require('chai');
+const { buildItemObject } = require('../test-utils');
 
-// Add your tests below:
+describe('User visits create', () => {
+    describe('post a new item', () => {
+        it('render the new post item after posting', () => {
+            const newItem = buildItemObject();
 
+            browser.url('create.html');
+
+            browser.setValue('#title-input', newItem.title);
+            browser.setValue('#description-input', newItem.description);
+            browser.setValue('#imageUrl-input', newItem.imageUrl);
+            browser.click('#submit-button');
+
+            assert.include(browser.getText('#items-container'), newItem.title);
+            assert.include(browser.getText('#items-container'), newItem.description);
+            assert.equal(browser.getAttribute('#items-container img', 'src'), newItem.imageUrl);
+        });
+    });
+});
