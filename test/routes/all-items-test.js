76bd9ffbe03abd697c189d6bc34152f48b631c01
Walmyr Filho <wlsf82@gmail.com>
2017-12-17 17:22:59 +0100

    update test indentation and remove duplicated code (require helper instead)

--- test/routes/all-items-test.js
+++ test/routes/all-items-test.js
@@ -1,48 +1,37 @@
-const {assert} = require('chai');
+const { assert } = require('chai');
 const request = require('supertest');
-const {jsdom} = require('jsdom');
+const { jsdom } = require('jsdom');
 
 const app = require('../../app');
 
-const {parseTextFromHTML, seedItemToDatabase} = require('../test-utils');
-const {connectDatabaseAndDropData, diconnectDatabase} = require('../setup-teardown-utils');
-
-const findImageElementBySource = (htmlAsString, src) => {
-  const image = jsdom(htmlAsString).querySelector(`img[src="${src}"]`);
-  if (image !== null) {
-    return image;
-  } else {
-    throw new Error(`Image with src "${src}" not found in HTML string`);
-  }
-};
+const { parseTextFromHTML, seedItemToDatabase, findImageElementBySource } = require('../test-utils');
+const { connectDatabaseAndDropData, diconnectDatabase } = require('../setup-teardown-utils');
 
 describe('Server path: /', () => {
 
-  beforeEach(connectDatabaseAndDropData);
+    beforeEach(connectDatabaseAndDropData);
 
-  afterEach(diconnectDatabase);
+    afterEach(diconnectDatabase);
 
-  describe('GET', () => {
-    it('renders an item with a title and image', async () => {
-      const item = await seedItemToDatabase();
+    describe('GET', () => {
+        it('renders an item with a title and image', async () => {
+            const item = await seedItemToDatabase();
 
-      const response = await request(app)
-      .get(`/`);
+            const response = await request(app).get(`/`);
+            const imageElement = findImageElementBySource(response.text, item.imageUrl);
 
-      assert.include(parseTextFromHTML(response.text, '.item-title'), item.title);
-      const imageElement = findImageElementBySource(response.text, item.imageUrl);
-      assert.equal(imageElement.src, item.imageUrl);
+            assert.include(parseTextFromHTML(response.text, '.item-title'), item.title);
+            assert.equal(imageElement.src, item.imageUrl);
+        });
+
+        it('renders all items from the database', async () => {
+            const firstItem = await seedItemToDatabase({title: 'Item1'});
+            const secondItem = await seedItemToDatabase({title: 'Item2'});
+
+            const response = await request(app).get(`/`);
+
+            assert.include(parseTextFromHTML(response.text, `#item-${firstItem._id} .item-title`), firstItem.title);
+            assert.include(parseTextFromHTML(response.text, `#item-${secondItem._id} .item-title`), secondItem.title);
+        });
     });
-
-    it('renders all items from the database', async () => {
-      const firstItem = await seedItemToDatabase({title: 'Item1'});
-      const secondItem = await seedItemToDatabase({title: 'Item2'});
-
-      const response = await request(app)
-        .get(`/`);
-
-      assert.include(parseTextFromHTML(response.text, `#item-${firstItem._id} .item-title`), firstItem.title);
-      assert.include(parseTextFromHTML(response.text, `#item-${secondItem._id} .item-title`), secondItem.title);
-    });
-  });
 });
