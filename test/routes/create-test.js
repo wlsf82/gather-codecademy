fe0b3467f66d1258ef0c5c1630ff363d4aefbdd4
Walmyr Filho <wlsf82@gmail.com>
2017-12-17 17:25:13 +0100

    indentation fixes, remove duplication (require helper instead), and add tests

--- test/routes/create-test.js
+++ test/routes/create-test.js
@@ -1,29 +1,43 @@
-const {assert} = require('chai');
+const { assert } = require('chai');
 const request = require('supertest');
-const {jsdom} = require('jsdom');
+const { jsdom } = require('jsdom');
 
 const app = require('../../app');
 const Item = require('../../models/item');
 
-const {parseTextFromHTML, buildItemObject} = require('../test-utils');
-const {connectDatabaseAndDropData, diconnectDatabase} = require('../setup-teardown-utils');
-
-const findImageElementBySource = (htmlAsString, src) => {
-  const image = jsdom(htmlAsString).querySelector(`img[src="${src}"]`);
-  if (image !== null) {
-    return image;
-  } else {
-    throw new Error(`Image with src "${src}" not found in HTML string`);
-  }
-};
+const { parseTextFromHTML, buildItemObject, findImageElementBySource } = require('../test-utils');
+const { connectDatabaseAndDropData, diconnectDatabase } = require('../setup-teardown-utils');
 
 describe('Server path: /items/create', () => {
-  const itemToCreate = buildItemObject();
+    const itemToCreate = buildItemObject();
 
-  beforeEach(connectDatabaseAndDropData);
+    beforeEach(connectDatabaseAndDropData);
 
-  afterEach(diconnectDatabase);
+    afterEach(diconnectDatabase);
 
-  // Write your describe blocks below:
+    describe('GET', () => {
+        it('renders empty input fields', async () => {
+            const response = await request(app).get('/items/create');
 
+            assert.equal(parseTextFromHTML(response.text, 'input#title-input'), '');
+            assert.equal(parseTextFromHTML(response.text, 'textarea#description-input'), '');
+            assert.equal(parseTextFromHTML(response.text, 'input#imageUrl-input'), '');
+        });
+    });
+
+    describe('POST', () => {
+        it('creates a new item and renders it', async () => {
+            const itemToCreate = await buildItemObject();
+
+            const response = await request(app)
+                .post('/items/create')
+                .type('form')
+                .send(itemToCreate);
+
+            const imageElement = findImageElementBySource(response.text, itemToCreate.imageUrl);
+
+            assert.include(parseTextFromHTML(response.text, '.item-title'), itemToCreate.title);
+            assert.equal(imageElement.src, itemToCreate.imageUrl);
+        });
+    });
 });
