971eb64096a221bb199c73d4dd820e6e6f6d507e
Walmyr Filho <wlsf82@gmail.com>
2017-12-17 17:26:17 +0100

    update test indentation and add spaces on requires to improve readability

--- test/routes/single-item-test.js
+++ test/routes/single-item-test.js
@@ -1,16 +1,16 @@
-const {assert} = require('chai');
+const { assert } = require('chai');
 const request = require('supertest');
 
 const app = require('../../app');
 
-const {parseTextFromHTML, seedItemToDatabase} = require('../test-utils');
-const {connectDatabaseAndDropData, diconnectDatabase} = require('../setup-teardown-utils');
+const { parseTextFromHTML, seedItemToDatabase } = require('../test-utils');
+const { connectDatabaseAndDropData, diconnectDatabase } = require('../setup-teardown-utils');
 
 describe('Server path: /items/:id', () => {
-  beforeEach(connectDatabaseAndDropData);
+    beforeEach(connectDatabaseAndDropData);
 
-  afterEach(diconnectDatabase);
+    afterEach(diconnectDatabase);
 
-  // Write your test blocks below:
-  
+    // Write your test blocks below:
+
 });
