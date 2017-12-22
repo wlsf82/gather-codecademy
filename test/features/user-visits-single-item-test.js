bc76f56e1af9ba760fceb28ccfc9fe4f173193fe
Walmyr Filho <wlsf82@gmail.com>
2017-12-22 17:26:09 +0000

    add feature test for single item

---
+++ test/features/user-visits-single-item-test.js
@@ -0,0 +1,22 @@
+const { assert } = require('chai');
+const { buildItemObject } = require('../test-utils');
+
+describe('User visits a single item', () => {
+    const itemToCreate = buildItemObject();
+
+    beforeEach(() => {
+        browser.url('/items/create');
+
+        browser.setValue('#title-input', itemToCreate.title);
+        browser.setValue('#description-input', itemToCreate.description);
+        browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
+        browser.click('#submit-button');
+    });
+
+    it('renders item title and description', () => {
+        browser.click('.item-card a');
+
+        assert.equal(browser.getText('#item-title'), itemToCreate.title);
+        assert.equal(browser.getText('#item-description'), itemToCreate.description);
+    });
+});
