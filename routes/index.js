311315857791aede3846b44e282de62590da84c9
Walmyr Filho <wlsf82@gmail.com>
2018-01-01 11:45:35 +0100

    fix post route for update item

--- routes/index.js
+++ routes/index.js
@@ -54,6 +54,7 @@ router.get("/items/:id/update", async (req, res, next) => {
 });
 
 router.post("/items/:id/update", async (req, res, next) => {
+    const item = await Item.findById({_id: req.params.id});
     const { title, description, imageUrl } = req.body;
 
     const updatedItem = new Item({
@@ -65,7 +66,16 @@ router.post("/items/:id/update", async (req, res, next) => {
     updatedItem.validateSync();
 
     if (updatedItem.errors) {
-        res.status(400).render("update", {updatedItem});
+        if (updatedItem.errors.title) {
+            item.title = ""
+        }
+        if (updatedItem.errors.description) {
+            item.description = ""
+        }
+        if (updatedItem.errors.imageUrl) {
+            item.imageUrl = ""
+        }
+        res.status(400).render("update", { updatedItem, item });
     } else {
         await Item.findOneAndUpdate({_id: req.params.id}, req.body, (error) => {
             if (error) return res.send(error);
